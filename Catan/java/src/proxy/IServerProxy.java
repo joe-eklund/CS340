f874a3da89b8aef30291b4e98c350f3c0e89845e
Joshua Grigg <joshuagrigg@hotmail.com>
2014-09-17 11:56:22 -0600

    Added Abstract Class for Server Proxy

--- Catan/java/src/proxy/IServerProxy.java
+++ Catan/java/src/proxy/IServerProxy.java
@@ -1,24 +1,163 @@
 package proxy;
 
-import java.net.HttpURLConnection;
+import java.util.List;
+import java.util.Map;
 
-public interface IServerProxy {
+import shared.definitions.ResourceType;
+
+public interface IServerProxy {	
+	
 	//Josh Begin
-	void loginUser();
-	void registerUser();
+	
+	/*
+	 * Logs an existing user into the game server
+	 * 
+	 * @pre none
+	 * 
+	 * @post
+	 */
+	void loginUser(String username, String password);
+	
+	/*
+	 *  This call performs the following 2 actions:
+	 *  1)Creates a new user account
+	 *	2) Logs the new user into the game server
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 * 
+	 */
+	void registerUser(String username, String password);
+	
+	/*
+	 * This call returns information about all of the current games
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 * 
+	 */
 	void listGames();
-	void createGame();
-	void joinGame();
+	
+	/*
+	 * Creates an empty game on the server
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 * 
+	 */
+	void createGame(String name);
+	
+	/*
+	 * Adds a player to the game and sets their game cookie
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 * 
+	 */
+	void joinGame(String color, int gameID);
+	
+	/*
+	 * Fetches the JSON for the model of the current game.
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 * 
+	 */
 	void getGameModel();
+	
+	/*
+	 * Resets the game to how it was after all the players joined
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 * 
+	 */
 	void resetGame();
+	
+	/*
+	 * Gets a list of all the commands played on a game
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 * 
+	 */
 	void getGameCommands();
-	void postGameCommands();
+	
+	/*
+	 * Applies a list of commands to the current game.
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 * 
+	 */
+	void postGameCommands(String commands);
+	
+	/*
+	 * Lists the available AI types that may be added to a game
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 * 
+	 */
 	void listAI();
+	
+	/*
+	 * Adds an AI to the game
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 * 
+	 */
 	void addAI();
-	void changeLogLevel();
-	void sendChat();
-	void acceptTrade();
-	void discardCards();
+	
+	/*
+	 * Sets the server’s logging level
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 * 
+	 */
+	void changeLogLevel(String logLevel);
+	
+	/*
+	 * Sends chat message to game chat log
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 * 
+	 */
+	void sendChat(String message);
+	
+	/*
+	 * Accept another player's trade offer
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 * 
+	 */
+	void acceptTrade(boolean willAccept);
+	
+	/*
+	 * Discards cards from players hand
+	 * 
+	 * @pre
+	 * 
+	 * @post
+	 */
+	void discardCards(List<ResourceType> resources, Map<ResourceType, Integer> resourceHand);
 	//Josh End
 	
 	// Taylor Begin
