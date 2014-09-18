7e3f510128ca1932bc34caebe231ba1d3e66cd15
Joshua Grigg <joshuagrigg@hotmail.com>
2014-09-17 21:38:02 -0600

    Added Skeleton & Comments for Poller

--- Catan/java/src/client/Poller/Poller.java
+++ Catan/java/src/client/Poller/Poller.java
@@ -1,3 +1,44 @@
+
 public class Poller {
+	private Timer poller;
 	
+	/**
+	 * @param proxy: server proxy
+	 * @param model: catan client side model to be updated every [secondsPollInterval] seconds
+	 * @param secondsPollInterval: invterval in seconds at which server will be polled to update client catan model
+	 */
+	public Poller(AProxyServer proxy, ClientModel model, int secondsPollInterval) {
+		
+	}
+	
+	
+	/**
+	 * starts poller
+	 * 
+	 * @pre 
+	 *  none
+	 *  
+	 * @post
+	 * 	if poller is not already polling:
+	 * 	   1) starts poller
+	 * 
+	 */
+	public startUpdates() {
+		
+	}
+	
+	
+	/**
+	 * stops poller
+	 * 
+	 * @pre
+	 *  none
+	 * 
+	 * @post
+	 * 	if poller is already polling:
+	 *     1) stops poller
+	 */
+	public stopUpdates() {
+		
+	}
 }
