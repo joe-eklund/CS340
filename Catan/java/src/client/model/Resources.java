b8717c9e5ae43ec28a0337fa4ae60cb8574501a1
Taylor Bell <taylorcgbell@gmail.com>
2014-09-30 19:52:21 -0600

    new resources
    new resources

---
+++ Catan/java/src/client/model/Resources.java
@@ -0,0 +1,59 @@
+package client.model;
+
+public class Resources {
+	public int brick;
+	public int ore;
+	public int sheep;
+	public int wheat;
+	public int wood;
+	
+	public Resources() {
+		brick=0;
+		wheat=0;
+		ore=0;
+		wood=0;
+		sheep=0;
+	}
+	public Resources(int brick, int wheat, int ore, int wood, int sheep) {
+		this.brick = brick;
+		this.wheat = wheat;
+		this.ore = ore;
+		this.wood = wood;
+		this.sheep = sheep;
+	}
+	public int getBrick() {
+		return brick;
+	}
+	public void setBrick(int brick) {
+		this.brick = brick;
+	}
+	public int getOre() {
+		return ore;
+	}
+	public void setOre(int ore) {
+		this.ore = ore;
+	}
+	public int getSheep() {
+		return sheep;
+	}
+	public void setSheep(int sheep) {
+		this.sheep = sheep;
+	}
+	public int getWheat() {
+		return wheat;
+	}
+	public void setWheat(int wheat) {
+		this.wheat = wheat;
+	}
+	public int getWood() {
+		return wood;
+	}
+	public void setWood(int wood) {
+		this.wood = wood;
+	}
+	
+	public int totalResourcesCount() {
+		return brick + ore + sheep + wheat + wood;
+	}
+}
+
