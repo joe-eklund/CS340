bb5249087e23f9fc13db1facbdf8a8ece8f81174
Chad-Barlow <triforce.dragon@gmail.com>
2014-10-04 06:30:48 -0600

    Finished the Client communicator, Comments, and functionality and all! :)

--- Catan/java/src/proxy/ClientCommunicator.java
+++ Catan/java/src/proxy/ClientCommunicator.java
@@ -11,12 +11,63 @@ import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
+/**Class Used for communicating with the server. It runs all the various game commands and communicates results back to the proxy
+ * @author Chad
+ */
 public class ClientCommunicator implements ICommunicator {
 
-	TranslatorJSON jsonTrans;
-	String Host;
-	int Port;
+	private TranslatorJSON jsonTrans;
+	private String Host;
+	private int Port;
 	
+	/**@Obvious
+	 * @return the jsonTrans
+	 */
+	public TranslatorJSON getJsonTrans() {
+		return jsonTrans;
+	}
+
+	/**@Obvious
+	 * @param jsonTrans the jsonTrans to set
+	 */
+	public void setJsonTrans(TranslatorJSON jsonTrans) {
+		this.jsonTrans = jsonTrans;
+	}
+
+	/**@Obvious
+	 * @return the host
+	 */
+	public String getHost() {
+		return Host;
+	}
+
+	/**@Obvious
+	 * @param host the host to set
+	 */
+	public void setHost(String host) {
+		Host = host;
+	}
+
+	/**@Obvious
+	 * @return the port
+	 */
+	public int getPort() {
+		return Port;
+	}
+
+	/**@Obvious
+	 * @param port the port to set
+	 */
+	public void setPort(int port) {
+		Port = port;
+	}
+
+	/**
+	 * Basic constructor for the ClientCommunicator
+	 * @param host
+	 * @param port
+	 * @param jsonTrans
+	 */
 	public ClientCommunicator(String host, int port, TranslatorJSON jsonTrans) {
 		super();
 		Host = host;
@@ -25,7 +76,8 @@ public class ClientCommunicator implements ICommunicator {
 	}
 
 	/**Starts the request from the server given the information from the proxy. Starts by packaging up the info and having the translator change it to json. Then takes the json object with the request type and sends it to the server. 
-	 * 
+	 * @pre A list of headers most be put together, plus all the other necessary information for boxing up a package to be sent to the server
+	 * @post If all conditions are right, and execution is normal, A commandResponse object packaged up with all necessary data is sent back to proxy. 
 	 * @param commandName
 	 * @param commandParameters
 	 * @return ICommandResponse from the server
@@ -57,8 +109,9 @@ public class ClientCommunicator implements ICommunicator {
 		return serverResponse;
 	}
 	
-	/**
-	 * 
+	/**Preforms a get operation to the server
+	 * @pre The Connection to the server must all ready be set up with the necessary headers
+	 * @post Response data is returned. 
 	 * @param headers 
 	 * @param urlPath
 	 * @throws ClientException
@@ -93,14 +146,14 @@ public class ClientCommunicator implements ICommunicator {
 		}
 		catch (IOException e) { // IO ERROR
 			System.err.print("Unable to doGet\n");
-			
-			//throw new ClientException("FAILURE");
 		}
 		return result;
 	}
 	
 	/**Make HTTP POST request to the specified URL, 
 	 *passing in the specified postData object
+	 * @pre The connection will  have already been set up, and the jsonObject has been packaged for being sent to the server
+	 * @post The right data is put onto the output stream
 	 * @param urlPath
 	 * @param postData
 	 * @throws ClientException
@@ -140,8 +193,6 @@ public class ClientCommunicator implements ICommunicator {
 		}
 		catch (IOException e) { // IO ERROR
 			System.err.print("Unable to doPost\n");
-			
-			//throw new ClientException("FAILURE");
 		}
 		return result;
 	}
