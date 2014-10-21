7338db61509a99f74c0404c9d55713d3b713b98d
Joshua Grigg <joshuagrigg@hotmail.com>
2014-10-20 21:59:10 -0600

    PlayingState Polling Now Functional

--- Catan/java/src/shared/states/Playing.java
+++ Catan/java/src/shared/states/Playing.java
@@ -1,7 +1,15 @@
 package shared.states;
 
+import shared.ServerMethodResponses.GetGameModelResponse;
+import client.presenter.IPresenter;
+
 public class Playing extends GamePlay {
 	public Playing() {
 		super("Playing");
+	}
+	
+	@Override 
+	public GetGameModelResponse getGameModel(IPresenter presenter) {
+		return presenter.getProxy().getGameModel(presenter.getVersion(), presenter.getCookie());
 	}
 }
