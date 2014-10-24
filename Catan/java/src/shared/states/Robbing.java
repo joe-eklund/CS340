8c5da803997509a643f71c2630c3f979355a3229
jeklund <joseph.eklund@comcast.net>
2014-10-24 02:16:15 -0600

    more stuff

--- Catan/java/src/shared/states/Robbing.java
+++ Catan/java/src/shared/states/Robbing.java
@@ -12,19 +12,5 @@ public class Robbing extends GamePlay {
 	@Override
 	public void robPlayer(IPresenter presenter, int playerIndex, int victimIndex,HexLocation location) {
 		MoveResponse response=presenter.getProxy().robPlayer(playerIndex, victimIndex, location, presenter.getCookie());
-		if(response != null && response.isSuccessful()) {
-			presenter.updateServerModel(response.getGameModel());
-			try {
-			    Thread.sleep(1000); //Don't even ask
-			} catch(InterruptedException ex) {
-			    Thread.currentThread().interrupt();
-			}
-			presenter.setVersion(presenter.getVersion()-1);
-		}
-		else {
-			System.err.println("Error robbing player in Robbing state.");
-		}
-				
-		
 	}
 }
