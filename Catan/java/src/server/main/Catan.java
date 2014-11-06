1c93641976d2259901fa23f9d307f507affa67cf
Joshua Grigg <joshuagrigg@hotmail.com>
2014-11-05 17:07:08 -0700

    Better Error Checking for Server Main

--- Catan/java/src/server/main/Catan.java
+++ Catan/java/src/server/main/Catan.java
@@ -5,6 +5,8 @@ import server.httpServer.ServerCommunicator;
 import server.users.UsersFacade;
 
 public class Catan {
+	private final static int MAX_PORT = 65535;
+	private final static int MIN_PORT = 0;
 
 	public static void main(String[] args) {
     	int portNumber = -1;
@@ -12,17 +14,21 @@ public class Catan {
 			try {
 				portNumber = Integer.parseInt(args[0]);
 			} catch (NumberFormatException e) {
-				//invalid input
+				System.err.println("Invalid input; port must be integer where port >= " + MIN_PORT + " && port <= " + MAX_PORT);
 			}
-		} 
-    	if(portNumber >= 0 && portNumber <= 65535) {
-    		TranslatorJSON translator = new TranslatorJSON();
-    		UsersFacade usersFacade = new UsersFacade();
-    		ServerCommunicator serverHandler = new ServerCommunicator(portNumber, translator, usersFacade);
-    		serverHandler.run();
-    	}
+			
+			if(portNumber >= MIN_PORT && portNumber <= MAX_PORT) {
+				TranslatorJSON translator = new TranslatorJSON();
+				UsersFacade usersFacade = new UsersFacade();
+				ServerCommunicator serverHandler = new ServerCommunicator(portNumber, translator, usersFacade);
+				serverHandler.run();
+			}
+			else {
+				System.err.println("Invalid port number; port must be >= " + MIN_PORT + " && port must be <= " + MAX_PORT);
+			}
+    	}	
     	else {
-			System.out.println("USAGE: java Server <port>");
+			System.err.println("USAGE: java Server <port>");
 		}
 	}
 	
