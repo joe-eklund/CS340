af77fd2865440bfdefb31b17d00db26176fd13e4
Epper Marshall <epper.marshall@gmail.com>
2014-11-07 14:43:44 -0700

    stubs skeleton

---
+++ Catan/Server/server/game/GameFacadeStub.java
@@ -0,0 +1,7 @@
+package server.game;
+
+import server.game.IGameFacade;
+
+public class GameFacadeStub implements IGameFacade {
+
+}
---
+++ Catan/Server/server/moves/MovesFacadeStub.java
@@ -0,0 +1,5 @@
+package server.moves;
+
+public class MovesFacadeStub implements IMovesFacade {
+
+}
