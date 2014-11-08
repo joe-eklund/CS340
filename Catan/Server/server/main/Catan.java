6b49b0aa86236d64e38aa22601914052c2ad3085
Joshua Grigg <joshuagrigg@hotmail.com>
2014-11-07 17:11:38 -0700

    Created command line parsing for server
    See comments at top of Server.catan.java for description of command
    line options

--- Catan/Server/server/main/Catan.java
+++ Catan/Server/server/main/Catan.java
@@ -1,35 +1,67 @@
 package server.main;
 
+import org.apache.commons.cli.*;
+
 import proxy.TranslatorJSON;
 import server.game.GameFacade;
+import server.game.IGameFacade;
 import server.games.GamesFacade;
+import server.games.IGamesFacade;
 import server.serverCommunicator.ServerCommunicator;
+import server.moves.IMovesFacade;
 import server.moves.MovesFacade;
+import server.users.IUsersFacade;
 import server.users.UsersFacade;
 import server.users.UsersFacadeStub;
+import server.util.IUtilFacade;
 import server.util.UtilFacade;
 
 public class Catan {
 	private final static int MAX_PORT = 65535;
 	private final static int MIN_PORT = 0;
 
+	/*
+	 * server will run without loading test stubs on port 8081 if no command line options are set
+	 * -p <portNumber> ::= set port to port number (i.e -p 39640 will set the server to listen on port 39640)
+	 * -t ::= run the server with test stubs (server will be initialized with mock data from stubs)
+	 * 
+	 * ==> "-p 39640 -t" will run the server on port 39640 with the testing data 
+	 */
 	public static void main(String[] args) {
-    	int portNumber = -1;
-    	if (args.length == 1) {
-			try {
-				portNumber = Integer.parseInt(args[0]);
-			} catch (NumberFormatException e) {
-				System.err.println("Invalid input; port must be integer where port >= " + MIN_PORT + " && port <= " + MAX_PORT);
+		
+		CommandLineParser parser = new GnuParser();
+		
+		Options options = new Options();
+		options.addOption("t", "test-data", false, "start server using testing stubs");
+		options.addOption("p", "port", true, "port on which server will listen");
+		
+		try {
+			TranslatorJSON translator = new TranslatorJSON();
+			IUsersFacade usersFacade = new UsersFacade();
+			IGamesFacade gamesFacade = new GamesFacade();
+			IGameFacade gameFacade = new GameFacade();
+			IMovesFacade movesFacade = new MovesFacade();
+			IUtilFacade utilFacade = new UtilFacade();
+			
+			CommandLine line = parser.parse(options, args);
+			
+			if(line.hasOption("t")) { // testing option ==> load server stubs
+				System.out.println("loading testing stubs");
+				
+				// set stubs here
+				usersFacade = new UsersFacadeStub();
+			}
+			
+			int portNumber = 8081;
+			if(line.hasOption("p")) {
+				try {
+					portNumber = Integer.parseInt(line.getOptionValue("p"));
+				} catch (NumberFormatException e) {
+					System.err.println("Invalid input; port must be integer where port >= " + MIN_PORT + " && port <= " + MAX_PORT);
+				}
 			}
 			
 			if(portNumber >= MIN_PORT && portNumber <= MAX_PORT) {
-				TranslatorJSON translator = new TranslatorJSON();
-				//UsersFacade usersFacade = new UsersFacade();
-				UsersFacadeStub usersFacade = new UsersFacadeStub();
-				GamesFacade gamesFacade = new GamesFacade();
-				GameFacade gameFacade = new GameFacade();
-				MovesFacade movesFacade = new MovesFacade();
-				UtilFacade utilFacade = new UtilFacade();
 				ServerCommunicator serverCommunicator = new ServerCommunicator(portNumber, translator, usersFacade, gamesFacade, gameFacade, movesFacade, utilFacade);
 				serverCommunicator.run();
 				System.out.println("Catan server listening on port: " + portNumber);
@@ -37,10 +69,12 @@ public class Catan {
 			else {
 				System.err.println("Invalid port number; port must be >= " + MIN_PORT + " && port must be <= " + MAX_PORT);
 			}
-    	}	
-    	else {
-			System.err.println("USAGE: java Catan <port>");
+
+		} catch(ParseException e) {
+			System.out.println("Error Starting Server");
+			System.err.println(e.getMessage());
 		}
+
 	}
 	
 }
