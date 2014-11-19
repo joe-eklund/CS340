badcfaa3ead59a7c476c4dba53e8b453c4e324be
Epper Marshall <epper.marshall@gmail.com>
2014-11-18 17:36:24 -0700

    dev card handlers

--- Catan/Server/server/serverCommunicator/RoadBuildingHandler.java
+++ Catan/Server/server/serverCommunicator/RoadBuildingHandler.java
@@ -1,9 +1,22 @@
 package server.serverCommunicator;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 import proxy.ITranslator;
+import server.cookie.Cookie;
+import server.cookie.CookieParams;
+import server.cookie.InvalidCookieException;
 import server.moves.IMovesFacade;
+import server.moves.InvalidMovesRequest;
+import shared.ServerMethodRequests.MonopolyDevRequest;
+import shared.ServerMethodRequests.RoadBuildingDevRequest;
+import shared.definitions.ServerModel;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -31,8 +44,67 @@ public class RoadBuildingHandler implements HttpHandler {
 	 */
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		// TODO Auto-generated method stub
+		System.out.println("In road building handler");
+		
+		String responseMessage = "";
+		
+		if(exchange.getRequestMethod().toLowerCase().equals("post")) {
+			try { 
+				//TODO verify cookie method
+				String unvalidatedCookie = exchange.getRequestHeaders().get("Cookie").get(0);
+				CookieParams cookie = Cookie.verifyCookie(unvalidatedCookie, translator);
+				
+				BufferedReader in = new BufferedReader(new InputStreamReader(exchange.getRequestBody()));
+				String inputLine;
+				StringBuffer requestJson = new StringBuffer();
+				while ((inputLine = in.readLine()) != null) {
+					requestJson.append(inputLine);
+				}
+				in.close();
+				
+				System.out.println(requestJson);
+				
+				RoadBuildingDevRequest request = (RoadBuildingDevRequest) translator.translateFrom(requestJson.toString(), RoadBuildingDevRequest.class);
+				exchange.getRequestBody().close();
+				
+				ServerModel serverModel = this.movesFacade.roadBuilding(request, cookie);
+				System.out.println("Request Accepted!");
+				// create cookie for user
+				List<String> cookies = new ArrayList<String>();
 
+				// send success response headers
+				exchange.getResponseHeaders().put("Set-cookie", cookies);
+				exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+				
+				responseMessage = translator.translateTo(serverModel);
+
+			} catch (InvalidCookieException | InvalidMovesRequest e) { // else send error message
+				System.out.println("unrecognized / invalid road building(dev) request");
+				responseMessage = e.getMessage();
+				exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+			}
+		}
+		else {
+			// unsupported request method
+			responseMessage = "Error: \"" + exchange.getRequestMethod() + "\" is no supported!";
+			exchange.sendResponseHeaders(HttpURLConnection.HTTP_BAD_REQUEST, 0);
+		}
+		
+		//set "Content-Type: text/plain" header
+		List<String> contentTypes = new ArrayList<String>();
+		String type = "text/plain";
+		contentTypes.add(type);
+		exchange.getResponseHeaders().put("Content-type", contentTypes);
+		
+		if (!responseMessage.isEmpty()) {
+			//send failure response message
+			OutputStreamWriter writer = new OutputStreamWriter(
+					exchange.getResponseBody());
+			writer.write(responseMessage);
+			writer.flush();
+			writer.close();
+		}
+		exchange.getResponseBody().close();
 	}
 
 }
