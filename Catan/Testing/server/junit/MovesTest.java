8dddd070c8d0b90587378127ac11c242dc0c0318
Epper Marshall <epper.marshall@gmail.com>
2014-11-24 18:02:08 -0700

    roll and send chat junits

--- Catan/Testing/server/junit/MovesTest.java
+++ Catan/Testing/server/junit/MovesTest.java
@@ -24,9 +24,12 @@ import server.serverCommunicator.RegisterUserHandler;
 import server.serverCommunicator.YearOfPlentyHandler;
 import server.users.IUsersFacade;
 import server.users.UsersFacadeStub;
+import shared.ServerMethodRequests.RollNumberRequest;
+import shared.ServerMethodRequests.SendChatRequest;
 import shared.ServerMethodRequests.UserRequest;
 import shared.ServerMethodRequests.YearOfPlentyDevRequest;
 import shared.definitions.ServerModel;
+import shared.model.Chat;
 
 public class MovesTest {
 	private IMovesFacade moves;
@@ -49,12 +52,59 @@ public class MovesTest {
 	
 	@Test
 	public void testSendChat() {
-		//player sends chat message-check appends
+		SendChatRequest request=new SendChatRequest(0, "Hello World");
+		ServerModel aGame=gamesList.get(1);
+		
+		try {
+			moves.sendChat(request, cookie);
+			assertEquals("Last chat message should equal 'Hello World'","Hello World",aGame.getChat().getMessages().get(aGame.getChat().getMessages().size()-1).getMessage());
+			assertEquals("Last chat message should have come from Bobby","Bobby",aGame.getChat().getMessages().get(aGame.getChat().getMessages().size()-1).getSource());
+			request=new SendChatRequest(1, "Hello back");
+			cookie = new CookieParams("Billy", "billy", 1, 1);
+			moves.sendChat(request, cookie);
+			assertEquals("Last chat message should equal 'Hello back'","Hello back",aGame.getChat().getMessages().get(aGame.getChat().getMessages().size()-1).getMessage());
+			assertEquals("Last chat message should have come from Billy","Billy",aGame.getChat().getMessages().get(aGame.getChat().getMessages().size()-1).getSource());
+		} catch (InvalidMovesRequest e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
 	@Test
 	public void testRollNumber() {
-		//player rolls number-check each player gets correct amount of resources
+		RollNumberRequest request=new RollNumberRequest(1, 0);
+		ServerModel aGame=gamesList.get(1);
+		
+		try {
+			moves.rollNumber(request, cookie);
+		} catch (InvalidMovesRequest e) {
+			assertEquals("Should have thrown 'Error: invalid roll number request'","Error: invalid roll number request",e.getMessage());
+		}
+		aGame=gamesList.get(2);
+		cookie=new CookieParams("Bobby", "bobby", 0, 2);
+		request=new RollNumberRequest(6,0);
+		int player0Resources=aGame.getPlayers().get(0).getResources().totalResourcesCount();
+		int player1Resources=aGame.getPlayers().get(1).getResources().totalResourcesCount();
+		int player2Resources=aGame.getPlayers().get(2).getResources().totalResourcesCount();
+		int player3Resources=aGame.getPlayers().get(3).getResources().totalResourcesCount();
+		try {
+			moves.rollNumber(request, cookie);
+			assertEquals("Player 0 should have more resources.",player0Resources+1,aGame.getPlayers().get(0).getResources().totalResourcesCount());
+			assertEquals("Player 1 should have more resources.",player1Resources+0,aGame.getPlayers().get(1).getResources().totalResourcesCount());
+			assertEquals("Player 2 should have more resources.",player2Resources+0,aGame.getPlayers().get(2).getResources().totalResourcesCount());
+			assertEquals("Player 3 should have more resources.",player3Resources+0,aGame.getPlayers().get(3).getResources().totalResourcesCount());
+			request=new RollNumberRequest(10,0);
+			player0Resources=aGame.getPlayers().get(0).getResources().totalResourcesCount();
+			player1Resources=aGame.getPlayers().get(1).getResources().totalResourcesCount();
+			player2Resources=aGame.getPlayers().get(2).getResources().totalResourcesCount();
+			player3Resources=aGame.getPlayers().get(3).getResources().totalResourcesCount();
+			moves.rollNumber(request, cookie);
+			assertEquals("Player 0 should have more resources.",player0Resources+0,aGame.getPlayers().get(0).getResources().totalResourcesCount());
+			assertEquals("Player 1 should have more resources.",player1Resources+1,aGame.getPlayers().get(1).getResources().totalResourcesCount());
+			assertEquals("Player 2 should have more resources.",player2Resources+0,aGame.getPlayers().get(2).getResources().totalResourcesCount());
+			assertEquals("Player 3 should have more resources.",player3Resources+1,aGame.getPlayers().get(3).getResources().totalResourcesCount());
+		} catch (InvalidMovesRequest e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
 	@Test
