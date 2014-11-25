4cdc224de4293d642b77b6a94a5d744a3da249b7
Joshua Grigg <joshuagrigg@hotmail.com>
2014-11-24 19:51:57 -0700

    Undid my merge conflict

--- Catan/Testing/server/junit/UserTest.java
+++ Catan/Testing/server/junit/UserTest.java
@@ -3,32 +3,21 @@ package server.junit;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
-
 import org.junit.Before;
 import org.junit.Test;
 
-import proxy.ClientCommunicator;
-import proxy.CommandResponse;
-import proxy.Pair;
-import proxy.RequestType;
-import proxy.TranslatorJSON;
 import server.commands.users.UsersCommandLog;
-import server.main.Catan;
 import server.users.IUsersFacade;
 import server.users.UsersFacadeStub;
 import shared.ServerMethodRequests.UserRequest;
-import shared.definitions.User;
 
 public class UserTest {
 	private IUsersFacade user;
-	private TranslatorJSON jsonTrans;
 	private UsersCommandLog userCommands;
 	
 	@Before 
 	public void setUp() { 
 		user = new UsersFacadeStub(); 
-		jsonTrans=new TranslatorJSON();
 		userCommands = new UsersCommandLog();
 		userCommands.setFacade(user);
 	}
@@ -84,32 +73,5 @@ public class UserTest {
 		register = user.registerUser(new UserRequest("bobbbyyy","bobby"));
 		assertEquals("Invalid registration: username less than 3 characters in length", -1, register);
 	}
-	
-	@SuppressWarnings("static-access")
-	@Test
-	public void testUserHandlers() {
-		// start server in testing mode for checking handlers and user registration
-		Catan game=new Catan();
-		game.main(new String[]{"-p","8080","-t"});
-		
-		// use client communicator to test handlers
-		ClientCommunicator client=new ClientCommunicator("localhost",8080,jsonTrans);
-		
-		CommandResponse response = client.executeCommand(RequestType.POST, new ArrayList<Pair<String,String>>(), "user/register", new User("Ji","jimmy"), null);
-		assertTrue("Username needs to be greater than length of 2",response.getResponseMessage().equals("Don't trust your client -- they have violated the server API contract: invalid username and/or password configuration."));
-		response = client.executeCommand(RequestType.POST, new ArrayList<Pair<String,String>>(), "user/register", new User("Tyrannosaurus","trexe"), null);
-		assertTrue("Username needs to be less than length of 8",response.getResponseMessage().equals("Don't trust your client -- they have violated the server API contract: invalid username and/or password configuration."));
-		response = client.executeCommand(RequestType.POST, new ArrayList<Pair<String,String>>(), "user/register", new User("Jimmy","jimm"), null);
-		assertTrue("Password needs to be greater than length of 4",response.getResponseMessage().equals("Don't trust your client -- they have violated the server API contract: invalid username and/or password configuration."));
-		response = client.executeCommand(RequestType.POST, new ArrayList<Pair<String,String>>(), "user/register", new User("Jimmy","$ma_r7"), null);
-		assertTrue("Password shouldn't contain invalid characters",response.getResponseMessage().equals("Don't trust your client -- they have violated the server API contract: invalid username and/or password configuration."));
-		testCommandLogExecuteAll();
-	}
-	
-	private void testCommandLogExecuteAll() {
-		UsersFacadeStub restoredUsers = new UsersFacadeStub();
-		userCommands.setFacade(restoredUsers);
-		userCommands.executeAll();
-		assertTrue("User and restoredUsers should be the same after commandLog.executeAll for User command log is executed on restoredUsers.", user.equals(restoredUsers));
-	}
+
 }
