237d799d31536685f77cf39dc8784ec085a6015d
Epper Marshall <epper.marshall@gmail.com>
2014-11-24 20:31:29 -0700

    json translator junits

--- Catan/Testing/client/junit/ClientCommunicatorTest.java
+++ Catan/Testing/client/junit/ClientCommunicatorTest.java
@@ -85,8 +85,8 @@ public class ClientCommunicatorTest {
 		//Pair<String, String> mockPair = new Pair<String, String>("Cookie", "catan.user=%7B%22name%22%3A%22Brooke%22%2C%22password%22%3A%22brooke%22%2C%22playerID%22%3A0%7D");
 		//headers.add(mockPair);
 		mockUser = new User("Brooke", "brooke");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "user/login", mockUser, null);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "user/login", mockUser, null);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -94,8 +94,8 @@ public class ClientCommunicatorTest {
 		//Pair<String, String> mockPair = new Pair<String, String>("Cookie", "catan.user=%7B%22name%22%3A%22Bob%22%2C%22password%22%3A%22bob%22%2C%22playerID%22%3A0%7D");
 		//headers.add(mockPair);
 		mockUser = new User("Test", "TEST");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "user/register", mockUser, null);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "user/register", mockUser, null);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -103,8 +103,8 @@ public class ClientCommunicatorTest {
 		Pair<String, String> mockPair = new Pair<String, String>("Cookie", "catan.user=%7B%22name%22%3A%22Brooke%22%2C%22password%22%3A%22brooke%22%2C%22playerID%22%3A0%7D");
 		headers.add(mockPair);
 		mockUser = new User("Brooke", "brooke");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.GET, headers, "games/list", mockUser, GameDescription[].class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.GET, headers, "games/list", mockUser, GameDescription[].class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -112,8 +112,8 @@ public class ClientCommunicatorTest {
 		Pair<String, String> mockPair = new Pair<String, String>("Cookie", "catan.user=%7B%22name%22%3A%22Brooke%22%2C%22password%22%3A%22brooke%22%2C%22playerID%22%3A0%7D");
 		headers.add(mockPair);
 		CreateGameRequest data = new CreateGameRequest(true,true,true, "GAME OF DOOM!!");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "games/create", data, GameDescription.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "games/create", data, GameDescription.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	//@Test //This test causes problems Dont run until fixed
@@ -121,8 +121,8 @@ public class ClientCommunicatorTest {
 		Pair<String, String> mockPair = new Pair<String, String>("Cookie", "catan.user=%7B%22name%22%3A%22Brooke%22%2C%22password%22%3A%22brooke%22%2C%22playerID%22%3A0%7D");
 		headers.add(mockPair);
 		JoinGameRequest data = new JoinGameRequest(4, "green");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "games/join", data, RequestType.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "games/join", data, RequestType.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	//@Test
@@ -130,8 +130,8 @@ public class ClientCommunicatorTest {
 		Pair<String, String> mockPair = new Pair<String, String>("Cookie", "catan.user=%7B%22name%22%3A%22Brooke%22%2C%22password%22%3A%22brooke%22%2C%22playerID%22%3A0%7D");
 		headers.add(mockPair);
 		mockUser = new User("Brooke", "brooke");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.GET, headers, "game/model", mockUser, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.GET, headers, "game/model", mockUser, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -141,8 +141,8 @@ public class ClientCommunicatorTest {
 		headers.add(mockPair);
 		//headers.add(mockPair2);
 		ResetGameRequest data = null; //new ResetGameRequest();
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "game/reset", data, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "game/reset", data, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -152,8 +152,8 @@ public class ClientCommunicatorTest {
 		headers.add(mockPair);
 		headers.add(mockPair2);
 		mockUser = new User("Brooke", "brooke");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.GET, headers, "game/commands", null, LogEntry[].class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.GET, headers, "game/commands", null, LogEntry[].class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -166,8 +166,8 @@ public class ClientCommunicatorTest {
 		Pair<String, String> mockPair = new Pair<String, String>("Cookie", "catan.user=%7B%22name%22%3A%22Brooke%22%2C%22password%22%3A%22brooke%22%2C%22playerID%22%3A0%7D");
 		headers.add(mockPair);
 		mockUser = new User("Brooke", "brooke");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.GET, headers, "game/listAI", mockUser, String[].class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.GET, headers, "game/listAI", mockUser, String[].class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -175,8 +175,8 @@ public class ClientCommunicatorTest {
 		Pair<String, String> mockPair = new Pair<String, String>("Cookie", "catan.user=%7B%22name%22%3A%22Brooke%22%2C%22password%22%3A%22brooke%22%2C%22playerID%22%3A0%7D");
 		headers.add(mockPair);
 		AddAIRequest data = new AddAIRequest("LARGEST_ARMY");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "game/addAI", data, null);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "game/addAI", data, null);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -184,8 +184,8 @@ public class ClientCommunicatorTest {
 		Pair<String, String> mockPair = new Pair<String, String>("Cookie", "catan.user=%7B%22name%22%3A%22Brooke%22%2C%22password%22%3A%22brooke%22%2C%22playerID%22%3A0%7D");
 		headers.add(mockPair);
 		ChangeLogLevelRequest data = new ChangeLogLevelRequest("SEVERE");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "util/changeLogLevel", data, null);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "util/changeLogLevel", data, null);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	////Move Command Tests*****************
@@ -197,8 +197,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		SendChatRequest mockChatRequest = new SendChatRequest(1, "Sup duuuuuuuude!!!!");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/sendChat", mockChatRequest, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/sendChat", mockChatRequest, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -208,8 +208,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		RollNumberRequest data = new RollNumberRequest(5, 1);
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/rollNumber", data, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/rollNumber", data, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 //	@Test 
@@ -219,8 +219,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		SoldierDevRequest data = new SoldierDevRequest(1,2, new HexLocation(5,5));
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/robPlayer", data, RequestType.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/robPlayer", data, RequestType.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -230,8 +230,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		FinishTurnRequest data = new FinishTurnRequest(1);
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/finishTurn", data, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/finishTurn", data, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 //	@Test
@@ -241,8 +241,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		BuyDevCardRequest data = new BuyDevCardRequest(1);
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/buyDevCard", data, RequestType.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/buyDevCard", data, RequestType.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -252,8 +252,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		YearOfPlentyDevRequest data = new YearOfPlentyDevRequest(1, "sheep", "wood");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/Year_of_Plenty", data, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/Year_of_Plenty", data, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 //	@Test
@@ -263,8 +263,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		RoadBuildingDevRequest data = new RoadBuildingDevRequest(1, null, null);
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/Road_Building", data, RequestType.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/Road_Building", data, RequestType.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 //	@Test
@@ -274,8 +274,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		SoldierDevRequest data = new SoldierDevRequest(1,2, new HexLocation(5,5));
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/Soldier", data, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/Soldier", data, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -285,8 +285,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		MonopolyDevRequest data = new MonopolyDevRequest(1, "sheep");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/Monopoly", data, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/Monopoly", data, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -296,8 +296,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		MonumentDevRequest data = new MonumentDevRequest(1);
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/Monument", data, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/Monument", data, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -307,8 +307,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		BuildRoadRequest data = new BuildRoadRequest(1, new RoadLocation(0, 0, EdgeDirection.SouthEast), true);
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/buildRoad", data, RequestType.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/buildRoad", data, RequestType.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 //	@Test
@@ -319,8 +319,8 @@ public class ClientCommunicatorTest {
 		headers.add(game);		
 		VertexLocationRequest vertex = new VertexLocationRequest(1,1,VertexDirection.NorthWest);
 		BuildSettlementRequest data1 = new BuildSettlementRequest(1 ,vertex, true);
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/buildSettlement", data1, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/buildSettlement", data1, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 //	@Test
@@ -332,8 +332,8 @@ public class ClientCommunicatorTest {
 		VertexLocationRequest vertex = new VertexLocationRequest(3,4,VertexDirection.NorthWest);
 
 		BuildCityRequest data = new BuildCityRequest(1, vertex);
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/buildCity", data, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/buildCity", data, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -343,8 +343,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		OfferTradeRequest data = new OfferTradeRequest(11, new ResourceHand(0,0,0,0,0), 1);
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/offerTrade", data, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/offerTrade", data, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 //	@Test
@@ -354,8 +354,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		AcceptTradeRequest data = new AcceptTradeRequest(1, true);
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/acceptTrade", data, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/acceptTrade", data, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -365,8 +365,8 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		MaritimeTradeRequest data = new MaritimeTradeRequest(1,2,"sheep", "wood");
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/maritimeTrade", data, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/maritimeTrade", data, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 	
 	@Test
@@ -376,7 +376,7 @@ public class ClientCommunicatorTest {
 		headers.add(user);
 		headers.add(game);
 		DiscardCardsRequest data = new DiscardCardsRequest(new ResourceHand(0,-1,-1,0,0),1);
-		CommandResponse mockResponse = CCTestor.executeCommand(RequestType.POST, headers, "moves/discardCards", data, ServerModel.class);
-		assertTrue(mockResponse.getResponseCode() == 200);
+		CommandResponse response = CCTestor.executeCommand(RequestType.POST, headers, "moves/discardCards", data, ServerModel.class);
+		assertTrue(response.getResponseCode() == 200);
 	}
 }
