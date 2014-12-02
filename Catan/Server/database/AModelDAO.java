73c34d764d7a2548ed2675362a48af036cceea34
jeklund <joseph.eklund@comcast.net>
2014-12-02 16:50:54 -0700

    Javadocs for IDBFactory and AModelDAO.

--- Catan/Server/database/AModelDAO.java
+++ Catan/Server/database/AModelDAO.java
@@ -1,5 +1,38 @@
 package database;
 
-public abstract class AModelDAO implements IModelDAO {
+import java.io.Serializable;
 
+/**
+ * Abstract class for accessing data models stored in databases
+ * stored model domain ::= List<ServerModel> | List<User> | List<GameDescription
+ * 
+ */
+public abstract class AModelDAO implements IModelDAO {
+	
+	/**
+	 * 
+	 * @param model ::= model to be added to database
+	 * @pre none
+	 * @post model is saved to appropriate database table 
+	 */
+	public void save(Serializable model){
+		//TODO save the model to database
+	}
+	
+	/**
+	 * @pre none
+	 * @post returns object representing blob of associated data model to be cast to appropriate model
+	 */
+	public Object load(){
+		//TODO load object
+		return null;
+	}
+	
+	/**
+	 * @pre none
+	 * @post clears all rows in associated data model
+	 */
+	public void clear(){
+		//TODO clear all rows
+	}
 }
--- Catan/Server/database/IDBFactoryPlugin.java
+++ Catan/Server/database/IDBFactoryPlugin.java
@@ -1,10 +1,43 @@
 package database;
 
+/**
+ * Interface for getting the DAOs to access and execute commands on a database.
+ * Any database plugin that adheres to this API will work with the server.
+ */
 public interface IDBFactoryPlugin {
+	
+	/**
+	 * Gets the model DAO to access the database.
+	 * @param type	The type of database, such as sqlite or mongodb.
+	 * @return		AModelDAO object to access the model in the database.
+	 */
 	public AModelDAO getModelDAO(String type);
+	
+	/**
+	 * Gets the Move Command DAO to access the database.
+	 * @return	AMoveCommandDAO object to access the move commands in the database.
+	 */
 	public AMoveCommandDAO getMoveCommandDAO();
+	
+	/**
+	 * Gets the Non Move Command DAO to access the database.
+	 * @return	ANonMoveCommandDAO object to access the non move commands in the database.
+	 */
 	public ANonMoveCommandDAO getNonMoveCommandDAO();
+	
+	/**
+	 * Starts a command to the database.
+	 */
 	public void start();
+	
+	/**
+	 * Stops a command to the database.
+	 * @param success	True or false to execute the command to the database.
+	 */
 	public void stop(boolean success);
+	
+	/**
+	 * Clears all tables in the current database plugin being used.
+	 */
 	public void clearAllTables();
 }
