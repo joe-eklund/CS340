88db4d1c8f9ad27847c4171b39442ff2da1dc906
Taylor Bell <taylorcgbell@gmail.com>
2014-12-10 12:27:26 -0700

    framework for nonsql
    framework for nonsql

--- Catan/Server/database/NonSQLGameDescriptionDAO.java
+++ Catan/Server/database/NonSQLGameDescriptionDAO.java
@@ -1,5 +1,33 @@
 package database;
 
-public class NonSQLGameDescriptionDAO extends AModelDAO{
+import java.io.Serializable;
 
+public class NonSQLGameDescriptionDAO extends AModelDAO{
+private NonSQLPlugin db;
+	
+	public NonSQLGameDescriptionDAO(NonSQLPlugin nonSqlPlugin) {
+		db =  nonSqlPlugin;
+	}
+	/**
+	 * Saves the list of game descriptions(serialize it first) into the db-only one blob/row
+	 */
+	@Override
+	public void save(Serializable model){
+		//TODO save the model to database
+	}
+	
+	/**
+	 * Loads the blob representing the list of game descriptions
+	 */
+	@Override
+	public Serializable load(){
+		return null;
+	}
+	/**
+	 * Drop table, create empty table
+	 */
+	@Override
+	public void clear(){
+		//TODO clear all rows
+	}
 }
--- Catan/Server/database/NonSQLGameModelDAO.java
+++ Catan/Server/database/NonSQLGameModelDAO.java
@@ -1,5 +1,35 @@
 package database;
 
-public class NonSQLGameModelDAO extends AModelDAO{
+import java.io.Serializable;
 
+public class NonSQLGameModelDAO extends AModelDAO{
+	private NonSQLPlugin db;
+	
+	public NonSQLGameModelDAO(NonSQLPlugin nonSqlPlugin) {
+		db = nonSqlPlugin;
+	}
+	
+	/**
+	 * Saves the list of game models(serialize it first) into the db-only one blob/row
+	 */
+	@Override
+	public void save(Serializable model){
+		//TODO save the model to database
+	}
+	
+	/**
+	 * Loads the blob representing the list of game models
+	 */
+	@Override
+	public Serializable load(){
+		
+		return null;
+	}
+	/**
+	 * Drop table, create empty table
+	 */
+	@Override
+	public void clear(){
+		//TODO clear all rows
+	}
 }
